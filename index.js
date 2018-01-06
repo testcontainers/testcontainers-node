832c21a80fea3c479efd615056771c8f9f5560da
Cristian Greco <cristian.greco@triptease.com>
2018-01-06 19:31:27 +0000

    Instantiate image manager

--- index.js
+++ index.js
@@ -1,10 +1,12 @@
 const Docker = require('dockerode')
 
 const ContainerRegistry = require('./src/container-registry')
+const ImageManager = require('./src/image-manager')
 const ContainerManager = require('./src/container-manager')
 
 const docker = new Docker()
 const containerRegistry = new ContainerRegistry({ containers: [] })
-const containerManager = new ContainerManager({ docker, containerRegistry })
+const imageManager = new ImageManager({ docker })
+const containerManager = new ContainerManager({ docker, imageManager, containerRegistry })
 
 module.exports = containerManager
