ba245fbe7b4ef3b5130427f09580f6055769099e
Cristian Greco <cristianrgreco@gmail.com>
2018-01-06 18:20:54 +0000

    Add test for starting a container via the manager

--- src/container-manager.spec.js
+++ src/container-manager.spec.js
@@ -7,15 +7,30 @@ describe('ContainerManager', () => {
   let containerManager
 
   beforeEach(() => {
-    mockDocker = jest.fn()
-    mockImageManager = jest.fn()
-    mockContainerRegistry = { getContainers: jest.fn() }
+    mockDocker = { createContainer: jest.fn() }
+    mockImageManager = { exists: jest.fn() }
+    mockContainerRegistry = {
+      registerContainer: jest.fn(),
+      getContainers: jest.fn()
+    }
 
     containerManager = new ContainerManager({
       docker: mockDocker,
       imageManager: mockImageManager,
       containerRegistry: mockContainerRegistry
     })
+  })
+
+  it('should start a container', async () => {
+    const mockInspect = { Config: { id: '1' } }
+    const mockContainer = { start: jest.fn(), inspect: jest.fn(() => mockInspect) }
+    mockDocker.createContainer.mockReturnValueOnce(Promise.resolve(mockContainer))
+    mockImageManager.exists.mockReturnValueOnce(Promise.resolve(true))
+
+    const container = await containerManager.startContainer({ image: 'image' })
+
+    expect(container).toEqual({ id: '1' })
+    expect(mockContainerRegistry.registerContainer).toHaveBeenCalled()
   })
 
   it('should stop all containers', async () => {
