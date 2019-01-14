ab8451224d4274839551a651b87120638d24c14e
Cristian Greco <cristianrgreco@gmail.com>
2019-01-14 22:22:36 +0000

    Minor test cleanup

--- src/generic-container.test.ts
+++ src/generic-container.test.ts
@@ -4,11 +4,12 @@ import { GenericContainer } from "./generic-container";
 describe("GenericContainer", () => {
   it("should start and stop a container", async () => {
     const container = await new GenericContainer("tutum/hello-world").withExposedPorts(80).start();
-    const testUrl = `http://localhost:${container.getMappedPort(80)}`;
 
+    const testUrl = `http://localhost:${container.getMappedPort(80)}`;
     const containerResponse = await fetch(testUrl);
     expect(containerResponse.status).toBe(200);
 
     await container.stop();
+    return expect(fetch(testUrl)).rejects.toThrowError();
   }, 10_000);
 });
