9f9f7a053132574f378ec34647883b5cb11020be
Dominik Weidenfeld <dominik@weidenfeld.info>
2019-08-08 12:17:32 +0200

    feat(buildargs): export build parameters

--- src/index.ts
+++ src/index.ts
@@ -1,3 +1,14 @@
 export { GenericContainer } from "./generic-container";
 export { StartedTestContainer, StoppedTestContainer, TestContainer } from "./test-container";
 export { Wait } from "./wait";
+export {
+  abortOnExistingImage,
+  Options,
+  WithArgument,
+  withBuildArg,
+  withContext,
+  withDockerClientFactory,
+  withImageName,
+  withImageTag,
+  withUuid
+} from "./options";
