ad673465c0ca1678b57e0d134d1f8cee6b45c02f
Dominik Weidenfeld <dominik@weidenfeld.info>
2019-09-02 08:08:10 +0200

    fix(exports): added builder to exports

--- src/index.ts
+++ src/index.ts
@@ -1,3 +1,3 @@
-export { GenericContainer } from "./generic-container";
+export { GenericContainer, GenericContainerBuilder } from "./generic-container";
 export { TestContainer, StartedTestContainer, StoppedTestContainer } from "./test-container";
 export { Wait } from "./wait";
