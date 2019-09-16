b41b1be428b6e8e2ea2b15958405ec9167538f55
Dominik Weidenfeld <dominik@weidenfeld.info>
2019-09-16 09:22:36 +0200

    fix(export): export image builder correctly

--- src/index.ts
+++ src/index.ts
@@ -1,5 +1,5 @@
 export { GenericContainer } from "./generic-container";
 export { GenericContainerBuilder } from "./generic-container-builder";
-export { ImageBuilder } from "./image-name-builder";
+export { ImageBuilder } from "./image-builder";
 export { TestContainer, StartedTestContainer, StoppedTestContainer } from "./test-container";
 export { Wait } from "./wait";
