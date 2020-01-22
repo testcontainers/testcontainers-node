f0d0a1e571d4ab868e9b681302baee5de2b9b48a
Andres Correa Casablanca <castarco@coderspirit.xyz>
2020-01-22 20:09:26 +0100

    Expose getId through TestContainer interface

--- src/test-container.ts
+++ src/test-container.ts
@@ -3,6 +3,7 @@ import { BindMode, Command, ContainerName, Dir, EnvKey, EnvValue, ExecResult, Tm
 import { Host } from "./docker-client-factory";
 import { Port } from "./port";
 import { WaitStrategy } from "./wait-strategy";
+import { Id as ContainerId } from "./container";
 
 export interface TestContainer {
   start(): Promise<StartedTestContainer>;
@@ -35,6 +36,7 @@ export interface StartedTestContainer {
   getContainerIpAddress(): Host;
   getMappedPort(port: Port): Port;
   getName(): ContainerName;
+  getId(): ContainerId;
   exec(command: Command[]): Promise<ExecResult>;
 }
 
