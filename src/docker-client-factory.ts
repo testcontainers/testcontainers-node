05b24fc7a4f57627bf9809eb4a0b0bd1b75a97a0
Cristian Greco <cristianrgreco@gmail.com>
2020-07-07 17:49:12 +0100

    Get Docker host from modem

--- src/docker-client-factory.ts
+++ src/docker-client-factory.ts
@@ -1,7 +1,5 @@
 import defaultGateway from "default-gateway";
 import Dockerode from "dockerode";
-import fs from "fs";
-import url from "url";
 import { DockerClient, DockerodeClient } from "./docker-client";
 import log from "./logger";
 
@@ -17,23 +15,25 @@ export class DockerodeClientFactory implements DockerClientFactory {
   private readonly client: DockerClient;
 
   constructor() {
-    if (process.env.DOCKER_HOST) {
-      const { host, client } = this.fromDockerHost(process.env.DOCKER_HOST);
-      this.host = host;
-      this.client = client;
-    } else if (process.env.CODEBUILD_BUILD_NUMBER) {
-      const { host, client } = this.fromDefaults();
-      this.host = host;
-      this.client = client;
-    } else if (fs.existsSync("/.dockerenv")) {
-      const { host, client } = this.fromDockerWormhole();
-      this.host = host;
-      this.client = client;
+    const dockerode = new Dockerode();
+    const modem = dockerode.modem;
+
+    if (modem.host) {
+      this.host = modem.host;
+      log.info(`Using Docker host from modem: ${this.host}`);
     } else {
-      const { host, client } = this.fromDefaults();
-      this.host = host;
-      this.client = client;
+      const socketPath = modem.socketPath;
+      if (socketPath === "//./pipe/docker_engine") {
+        const { gateway } = defaultGateway.v4.sync();
+        this.host = gateway;
+        log.info(`Using Docker host from wormhole: ${this.host}, socket path is: ${socketPath}`);
+      } else {
+        this.host = "localhost";
+        log.info(`Using default Docker host: ${this.host}, socket path is: ${socketPath}`);
+      }
     }
+
+    this.client = new DockerodeClient(this.host, dockerode);
   }
 
   public getClient(): DockerClient {
@@ -42,41 +42,5 @@ export class DockerodeClientFactory implements DockerClientFactory {
 
   public getHost(): Host {
     return this.host;
-  }
-
-  private fromDefaults() {
-    log.info("Using Docker defaults");
-
-    const host = "localhost";
-    const dockerode = new Dockerode();
-    const client = new DockerodeClient(host, dockerode);
-
-    return { host, client };
-  }
-
-  private fromDockerHost(dockerHost: string) {
-    log.info(`Using Docker configuration from DOCKER_HOST: ${dockerHost}`);
-
-    const { hostname: host, port } = url.parse(dockerHost);
-    if (!host || !port) {
-      throw new Error(`Invalid format for DOCKER_HOST, found: ${dockerHost}`);
-    }
-
-    const dockerode = new Dockerode({ host, port });
-    const client = new DockerodeClient(host, dockerode);
-
-    return { host, client };
-  }
-
-  private fromDockerWormhole() {
-    log.info("Using Docker in Docker method");
-
-    const { gateway } = defaultGateway.v4.sync();
-
-    const host = gateway;
-    const dockerode = new Dockerode();
-    const client = new DockerodeClient(host, dockerode);
-
-    return { host, client };
   }
 }
