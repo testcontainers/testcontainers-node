f0ee19fd084d806143eb20ca96c1e5faad9d11e9
Cristian Greco <cristianrgreco@gmail.com>
2020-09-20 15:41:44 +0100

    Do not bind the same port twice

--- src/port-binder.test.ts
+++ src/port-binder.test.ts
@@ -11,4 +11,14 @@ describe("PortBinder", () => {
     expect(boundPorts.getBinding(1)).toBe(1000);
     expect(boundPorts.getBinding(2)).toBe(2000);
   });
+
+  it("should not produce the same port more than once", async () => {
+    const portClient = new FixedPortClient([1000, 1000, 2000]);
+    const portBinder = new PortBinder(portClient);
+
+    const boundPorts = await portBinder.bind([1, 2]);
+
+    expect(boundPorts.getBinding(1)).toBe(1000);
+    expect(boundPorts.getBinding(2)).toBe(2000);
+  });
 });
--- src/port-binder.ts
+++ src/port-binder.ts
@@ -3,13 +3,21 @@ import { Port } from "./port";
 import { PortClient, RandomPortClient } from "./port-client";
 
 export class PortBinder {
+  private readonly ports: Set<number> = new Set();
+
   constructor(private readonly portClient: PortClient = new RandomPortClient()) {}
 
   public async bind(ports: Port[]): Promise<BoundPorts> {
     const boundPorts = new BoundPorts();
 
     for (const port of ports) {
-      boundPorts.setBinding(port, await this.portClient.getPort());
+      let allocatedPort: Port;
+      do {
+        allocatedPort = await this.portClient.getPort();
+      } while (this.ports.has(allocatedPort));
+
+      this.ports.add(allocatedPort);
+      boundPorts.setBinding(port, allocatedPort);
     }
 
     return boundPorts;
