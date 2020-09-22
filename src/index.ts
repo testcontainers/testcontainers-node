6b7c0432cd729e239fc619d4f627c92c3a0b10de
Cristian Greco <cristianrgreco@gmail.com>
2020-09-22 21:01:22 +0100

    Add support for stopping a docker-compose environment

--- src/index.ts
+++ src/index.ts
@@ -1,13 +1,18 @@
-export { GenericContainer, GenericContainerBuilder } from "./generic-container";
 export { TestContainer, StartedTestContainer, StoppedTestContainer } from "./test-container";
-export { Network, StartedNetwork, StoppedNetwork } from "./network";
+export { GenericContainer, GenericContainerBuilder } from "./generic-container";
+
 export {
   DockerComposeEnvironment,
   StartedDockerComposeEnvironment,
   StoppedDockerComposeEnvironment,
+  DownedDockerComposeEnvironment,
 } from "./docker-compose-environment";
+
+export { Network, StartedNetwork, StoppedNetwork } from "./network";
+
 export { Wait } from "./wait";
 export { PullPolicy, DefaultPullPolicy, AlwaysPullPolicy } from "./pull-policy";
+
 export { KafkaContainer, StartedKafkaContainer } from "./modules/kafka/kafka-container";
 export { Neo4jContainer, StartedNeo4jContainer } from "./modules/neo4j/neo4j-container";
 export { ArangoDBContainer, StartedArangoContainer } from "./modules/arangodb/arangodb-container";
