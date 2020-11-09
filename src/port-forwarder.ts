8f25dc0dc3c7b9d18b67983ed3015498be9145af
Cristian Greco <cristianrgreco@gmail.com>
2020-11-09 21:11:57 +0000

    Use ssh-remote-port-forward lib

--- src/port-forwarder.ts
+++ src/port-forwarder.ts
@@ -1,26 +1,17 @@
-import { Client } from "ssh2";
+import { createSshConnection, SshConnection } from "ssh-remote-port-forward";
 import { DockerClient } from "./docker-client";
 import { log } from "./logger";
 import { GenericContainer } from "./generic-container";
 import { Port } from "./port";
 import { StartedTestContainer } from "./test-container";
-import net from "net";
-import { Host } from "./docker-client-instance";
 import { RandomUuid } from "./uuid";
 
 export class PortForwarder {
-  constructor(private readonly sshConnection: Client, private readonly container: StartedTestContainer) {}
+  constructor(private readonly sshConnection: SshConnection, private readonly container: StartedTestContainer) {}
 
   public async exposeHostPort(port: Port): Promise<void> {
     log.info(`Exposing host port ${port}`);
-    await new Promise((resolve, reject) => {
-      this.sshConnection.forwardIn("127.0.0.1", port, (err) => {
-        if (err) {
-          return reject(err);
-        }
-        resolve();
-      });
-    });
+    await this.sshConnection.remoteForward("localhost", port);
   }
 
   public getNetworkId(): string {
@@ -75,40 +66,9 @@ export class PortForwarderInstance {
     const port = container.getMappedPort(22);
 
     log.debug(`Connecting to Port Forwarder on ${host}:${port}`);
-    const connection = await this.createSshConnection(host, port, username, password);
+    const connection = await createSshConnection({ host, port, username, password });
+    connection.unref();
 
     return new PortForwarder(connection, container);
-  }
-
-  private static async createSshConnection(
-    host: Host,
-    port: Port,
-    username: string,
-    password: string
-  ): Promise<Client> {
-    return await new Promise((resolve) => {
-      const connection = new Client();
-      connection
-        .on("ready", () => {
-          log.debug("SSH connection established");
-          // @ts-ignore
-          connection._sock.unref();
-          resolve(connection);
-        })
-        .on("tcp connection", (info, accept) => {
-          const stream = accept();
-          stream
-            .on("end", () => log.debug("SSH connection ended"))
-            .on("error", (err: unknown) => log.warn(`SSH error: ${err}`))
-            .pause();
-
-          const socket = net.connect(info.destPort, info.destIP, () => {
-            stream.pipe(socket);
-            socket.pipe(stream);
-            stream.resume();
-          });
-        })
-        .connect({ host, port, username, password });
-    });
   }
 }
