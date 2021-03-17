787c63fc39899ecc5986b3748830201af0614ff9
Cristian Greco <cristianrgreco@gmail.com>
2021-03-17 21:40:56 +0000

    Refactor Dockerode out of tests

--- src/reaper.test.ts
+++ src/reaper.test.ts
@@ -2,11 +2,8 @@ import { GenericContainer } from "./generic-container";
 import { ReaperInstance } from "./reaper";
 import { getImagesRepoTags, getRunningContainerIds, getRunningNetworkIds } from "./test-helper";
 import { Network } from "./network";
-import Dockerode from "dockerode";
 import path from "path";
 import { RandomUuid } from "./uuid";
-
-const dockerode = new Dockerode();
 
 describe("Reaper", () => {
   jest.setTimeout(60_000);
@@ -16,9 +13,9 @@ describe("Reaper", () => {
 
     await ReaperInstance.stopInstance();
 
-    expect(await getRunningContainerIds(dockerode)).toContain(container.getId());
+    expect(await getRunningContainerIds()).toContain(container.getId());
     await new Promise((resolve) => setTimeout(resolve, 15_000));
-    expect(await getRunningContainerIds(dockerode)).not.toContain(container.getId());
+    expect(await getRunningContainerIds()).not.toContain(container.getId());
   });
 
   it("should remove networks", async () => {
@@ -26,9 +23,9 @@ describe("Reaper", () => {
 
     await ReaperInstance.stopInstance();
 
-    expect(await getRunningNetworkIds(dockerode)).toContain(network.getId());
+    expect(await getRunningNetworkIds()).toContain(network.getId());
     await new Promise((resolve) => setTimeout(resolve, 15_000));
-    expect(await getRunningNetworkIds(dockerode)).not.toContain(network.getId());
+    expect(await getRunningNetworkIds()).not.toContain(network.getId());
   });
 
   it("should remove images", async () => {
@@ -38,8 +35,8 @@ describe("Reaper", () => {
 
     await ReaperInstance.stopInstance();
 
-    expect(await getImagesRepoTags(dockerode)).toContain(imageId);
+    expect(await getImagesRepoTags()).toContain(imageId);
     await new Promise((resolve) => setTimeout(resolve, 15_000));
-    expect(await getImagesRepoTags(dockerode)).not.toContain(imageId);
+    expect(await getImagesRepoTags()).not.toContain(imageId);
   });
 });
