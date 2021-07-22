01d2357d83e33ca9533b3e222c9df1f98509e3f6
Cristian Greco <cristianrgreco@gmail.com>
2021-07-22 14:19:26 +0100

    Refactor runInContainer to use PassThrough

--- src/run-in-container.ts
+++ src/run-in-container.ts
@@ -1,6 +1,7 @@
 import { Command } from "./docker-client";
 import { log } from "./logger";
 import Dockerode from "dockerode";
+import { PassThrough } from "stream";
 
 export const runInContainer = async (
   dockerode: Dockerode,
@@ -12,25 +13,14 @@ export const runInContainer = async (
     const stream = await container.attach({ stream: true, stdout: true, stderr: true });
 
     const chunks: string[] = [];
+
     const promise = new Promise<void>((resolve) => {
       stream.on("end", () => resolve());
 
-      const outStream = {
-        write: (chunk: string) => {
-          const chunkStr = Buffer.from(chunk).toString().trim();
-          chunks.push(chunkStr);
-        },
-      };
+      const out = new PassThrough({ encoding: "utf-8" }).on("data", (chunk) => chunks.push(chunk.trim()));
+      const err = new PassThrough({ encoding: "utf-8" }).on("data", () => resolve(undefined));
 
-      const errStream = {
-        write: (chunk: string) => {
-          const chunkStr = Buffer.from(chunk).toString().trim();
-          log.warn(`Unexpected STDERR when running command in container: ${chunkStr}`);
-          resolve(undefined);
-        },
-      };
-
-      container.modem.demuxStream(stream, outStream, errStream);
+      container.modem.demuxStream(stream, out, err);
     });
 
     await container.start();
