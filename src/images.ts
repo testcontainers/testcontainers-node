fd1537c29e836a70b8b657cbb8158364da8c8754
Cristian Greco <cristianrgreco@gmail.com>
2021-08-03 10:24:37 +0100

    Minor refactor

--- src/images.ts
+++ src/images.ts
@@ -1,5 +1,2 @@
-export const REAPER_IMAGE =
-  process.env.RYUK_CONTAINER_IMAGE === undefined ? "testcontainers/ryuk:0.3.2" : process.env.RYUK_CONTAINER_IMAGE;
-
-export const SSHD_IMAGE =
-  process.env.SSHD_CONTAINER_IMAGE === undefined ? "testcontainers/sshd:1.0.0" : process.env.SSHD_CONTAINER_IMAGE;
+export const REAPER_IMAGE = process.env["RYUK_CONTAINER_IMAGE"] ?? "testcontainers/ryuk:0.3.2";
+export const SSHD_IMAGE = process.env["SSHD_CONTAINER_IMAGE"] ?? "testcontainers/sshd:1.0.0";
