23b6dedcdbd9210628ab1154c1c4195aec581e33
Ragan Webber <raganw@gmail.com>
2021-08-15 09:33:42 -0700

    Support starting a subset of docker-compose services (#252)
    Prior to this change, the `DockerComposeEnvironment().up()` method would
    start all services defined in the target `docker-compose.yml` file, and
    did not allow starting just a subset of services.
    
    This change adds support for optionally passing an array of service
    names to start to the `DockerComposeEnvironment.up()` method. If
    provided, the underlying `docker-compose` `upMany` method is used to
    start just that subset of services.

--- src/docker-compose/functions/docker-compose-up.ts
+++ src/docker-compose/functions/docker-compose-up.ts
@@ -1,14 +1,18 @@
 import { log } from "../../logger";
-import { upAll } from "docker-compose";
+import { upAll, upMany } from "docker-compose";
 import { defaultDockerComposeOptions } from "../default-docker-compose-options";
 import { DockerComposeOptions } from "../docker-compose-options";
 import { dockerComposeDown } from "./docker-compose-down";
 
-export const dockerComposeUp = async (options: DockerComposeOptions): Promise<void> => {
+export const dockerComposeUp = async (options: DockerComposeOptions, services?: Array<string>): Promise<void> => {
   log.info(`Upping DockerCompose environment`);
 
   try {
-    await upAll(defaultDockerComposeOptions(options));
+    if (services) {
+      await upMany(services, defaultDockerComposeOptions(options));
+    } else {
+      await upAll(defaultDockerComposeOptions(options));
+    }
     log.info(`Upped DockerCompose environment`);
   } catch (err) {
     log.error(`Failed to up DockerCompose environment: ${err}`);
