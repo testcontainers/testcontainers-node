9457acd1407551a38f08eb1975c7e1e81b126c5d
mmartinsky <michaelmartinsky@yahoo.com>
2021-09-28 23:31:13 -0400

    adds mysql module

---
+++ src/modules/mysql/mysql-container.test.ts
@@ -0,0 +1,60 @@
+import { createConnection } from "mysql2/promise";
+import { MySqlContainer } from "./mysql-container";
+
+describe("MySqlContainer", () => {
+  jest.setTimeout(180_000);
+
+  it("should work", async () => {
+    const container = await new MySqlContainer().start();
+
+    const client = await createConnection({
+      host: container.getHost(),
+      port: container.getPort(),
+      database: container.getDatabase(),
+      user: container.getUsername(),
+      password: container.getUserPassword(),
+    });
+
+    const [rows] = await client.execute("SELECT 1 as res");
+    expect(rows).toEqual([{ res: 1 }]);
+
+    await client.end();
+    await container.stop();
+  });
+
+  it("should set database", async () => {
+    const container = await new MySqlContainer().withDatabase("customDatabase").start();
+
+    const client = await createConnection({
+      host: container.getHost(),
+      port: container.getPort(),
+      database: container.getDatabase(),
+      user: container.getUsername(),
+      password: container.getUserPassword(),
+    });
+
+    const [rows] = await client.execute("SELECT DATABASE() as res");
+    expect(rows).toEqual([{ res: "customDatabase" }]);
+
+    await client.end();
+    await container.stop();
+  });
+
+  it("should set username", async () => {
+    const container = await new MySqlContainer().withUsername("customUsername").start();
+
+    const client = await createConnection({
+      host: container.getHost(),
+      port: container.getPort(),
+      database: container.getDatabase(),
+      user: container.getUsername(),
+      password: container.getUserPassword(),
+    });
+
+    const [rows] = await client.execute("SELECT CURRENT_USER() as res");
+    expect(rows).toEqual([{ res: "customUsername@%" }]);
+
+    await client.end();
+    await container.stop();
+  });
+});
---
+++ src/modules/mysql/mysql-container.ts
@@ -0,0 +1,88 @@
+import { GenericContainer } from "../../generic-container/generic-container";
+import { StartedTestContainer } from "../../test-container";
+import { RandomUuid } from "../../uuid";
+import { AbstractStartedContainer } from "../abstract-started-container";
+import { Port } from "../../port";
+
+export class MySqlContainer extends GenericContainer {
+  private database = "test";
+  private username = new RandomUuid().nextUuid();
+  private userPassword = new RandomUuid().nextUuid();
+  private rootPassword = new RandomUuid().nextUuid();
+
+  constructor(image = "mysql:8.0.26") {
+    super(image);
+  }
+
+  public withDatabase(database: string): this {
+    this.database = database;
+    return this;
+  }
+
+  public withUsername(username: string): this {
+    this.username = username;
+    return this;
+  }
+
+  public withRootPassword(rootPassword: string): this {
+    this.rootPassword = rootPassword;
+    return this;
+  }
+
+  public withUserPassword(userPassword: string): this {
+    this.userPassword = userPassword;
+    return this;
+  }
+
+  public async start(): Promise<StartedMySqlContainer> {
+    this.withExposedPorts(3306)
+      .withEnv("MYSQL_DATABASE", this.database)
+      .withEnv("MYSQL_ROOT_PASSWORD", this.rootPassword)
+      .withEnv("MYSQL_USER", this.username)
+      .withEnv("MYSQL_PASSWORD", this.userPassword)
+      .withStartupTimeout(120_000);
+
+    return new StartedMySqlContainer(
+      await super.start(),
+      this.database,
+      this.username,
+      this.userPassword,
+      this.rootPassword
+    );
+  }
+}
+
+export class StartedMySqlContainer extends AbstractStartedContainer {
+  private readonly port: Port;
+
+  constructor(
+    startedTestContainer: StartedTestContainer,
+    private readonly database: string,
+    private readonly username: string,
+    private readonly userPassword: string,
+    private readonly rootPassword: string
+  ) {
+    super(startedTestContainer);
+    this.port = startedTestContainer.getMappedPort(3306);
+  }
+
+  public getPort(): Port {
+    return this.port;
+  }
+
+  public getDatabase(): string {
+    return this.database;
+  }
+
+  public getUsername(): string {
+    return this.username;
+  }
+
+  public getUserPassword(): string {
+    return this.userPassword;
+  }
+
+  public getRootPassword(): string {
+    return this.rootPassword;
+  }
+}
