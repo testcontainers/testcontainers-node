a3d0faa0a6d5ffbf57902e3bf9ddfa472f22ac0a
Cristian Greco <cristianrgreco@gmail.com>
2021-11-13 21:46:32 +0000

    Test linter

--- src/docker-compose/functions/docker-compose-down.ts
+++ src/docker-compose/functions/docker-compose-down.ts
@@ -1,4 +1,4 @@
-import { down } from "docker-compose";
+import { down, run } from "docker-compose";
 import { log } from "../../logger";
 import { defaultDockerComposeOptions } from "../default-docker-compose-options";
 import { DockerComposeOptions } from "../docker-compose-options";
@@ -10,7 +10,7 @@ export const dockerComposeDown = async (options: DockerComposeOptions): Promise<
     await down({ ...defaultDockerComposeOptions(options), commandOptions: ["-v"] });
     log.info(`Downed DockerCompose environment`);
   } catch (err) {
-    const errorMessage = err.err || err.message || err;
+      const errorMessage = err.err || err.message || err;
     log.error(`Failed to down DockerCompose environment: ${errorMessage}`);
     throw new Error(errorMessage);
   }
