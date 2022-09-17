3dfc799def16c8e783fad2ab6d379a330a46f455
Cristian Greco <cristianrgreco@gmail.com>
2022-09-17 14:31:38 +0100

    Improve diagnostic logging for HostPort wait strategy (#397)
    * Improve diagnostic logging for HostPort wait strategy
    
    * Cleanup
    
    * Improve test names

--- src/port-check.ts
+++ src/port-check.ts
@@ -2,6 +2,7 @@ import { Socket } from "net";
 import { Port } from "./port";
 import { Host } from "./docker/types";
 import { execContainer } from "./docker/functions/container/exec-container";
+import { log } from "./logger";
 import Dockerode from "dockerode";
 
 export interface PortCheck {
@@ -33,6 +34,9 @@ export class HostPortCheck implements PortCheck {
 }
 
 export class InternalPortCheck implements PortCheck {
+  private isDistroless = false;
+  private commandOutputs = new Set<string>();
+
   constructor(private readonly container: Dockerode.Container) {}
 
   public async isBound(port: Port): Promise<boolean> {
@@ -42,7 +46,38 @@ export class InternalPortCheck implements PortCheck {
       ["/bin/sh", "-c", `nc -vz -w 1 localhost ${port}`],
       ["/bin/bash", "-c", `</dev/tcp/localhost/${port}`],
     ];
+
     const commandResults = await Promise.all(commands.map((command) => execContainer(this.container, command)));
-    return commandResults.some((result) => result.exitCode === 0);
+    const isBound = commandResults.some((result) => result.exitCode === 0);
+
+    if (!isBound) {
+      const shellExists = commandResults.some((result) => result.exitCode !== 126);
+      if (!shellExists) {
+        if (!this.isDistroless) {
+          this.isDistroless = true;
+          log.error(
+            `The HostPortWaitStrategy will not work on a distroless image, use an alternate wait strategy for container ${this.container.id}`
+          );
+        }
+      } else {
+        commandResults
+          .map((result) => ({ ...result, output: result.output.trim() }))
+          .filter((result) => result.exitCode !== 126 && result.output.length > 0)
+          .forEach((result) => {
+            if (!this.commandOutputs.has(this.commandOutputsKey(result.output))) {
+              log.trace(
+                `Port check result for container ${this.container.id} exit code ${result.exitCode}: ${result.output}`
+              );
+              this.commandOutputs.add(this.commandOutputsKey(result.output));
+            }
+          });
+      }
+    }
+
+    return isBound;
+  }
+
+  private commandOutputsKey(output: string) {
+    return `${this.container.id}:${output}`;
   }
 }
