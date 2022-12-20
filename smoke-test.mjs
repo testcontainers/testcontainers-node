0700e31ffc8781c2bba0580a54d58fe939757b12
Cristian Greco <cristianrgreco@gmail.com>
2022-12-20 20:15:43 +0000

    Refactor smoke-test

--- smoke-test.mjs
+++ smoke-test.mjs
@@ -1,4 +1,4 @@
-import { GenericContainer } from "testcontainers";
+import { GenericContainer } from "./dist/index.js";
 
 const container = await new GenericContainer("alpine:3.12").start();
 await container.stop();
