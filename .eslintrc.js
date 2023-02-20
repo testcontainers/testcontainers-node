62097cfd539ccf776c0e50ba969555ae07c7b3d9
Cristian Greco <cristianrgreco@gmail.com>
2023-02-20 10:14:30 +0000

    Add code linting to pipeline (#461)

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,18 +1,9 @@
 module.exports = {
-    root: true,
-    parser: '@typescript-eslint/parser',
-    plugins: [
-        '@typescript-eslint',
-    ],
-    extends: [
-        'prettier',
-        'eslint:recommended',
-        'plugin:@typescript-eslint/recommended',
-    ],
-    rules: {
-        "@typescript-eslint/ban-ts-comment": 0,
-        "@typescript-eslint/no-empty-interface": 0,
-        "@typescript-eslint/no-explicit-any": 0,
-        "@typescript-eslint/no-non-null-assertion": 0
-    }
+  root: true,
+  parser: "@typescript-eslint/parser",
+  plugins: ["prettier", "@typescript-eslint"],
+  extends: ["eslint:recommended", "plugin:@typescript-eslint/recommended", "plugin:prettier/recommended"],
+  rules: {
+    "prettier/prettier": "error",
+  },
 };
