935cd7d07d54bfe01025d22174c32402c91c54da
Eddú Meléndez <eddu.melendez@gmail.com>
2023-03-03 16:11:41 -0600

    Update testcontainers images using scarf

--- src/images.ts
+++ src/images.ts
@@ -1,2 +1,4 @@
-export const REAPER_IMAGE = process.env["RYUK_CONTAINER_IMAGE"] ?? "testcontainers/ryuk:0.3.4";
-export const SSHD_IMAGE = process.env["SSHD_CONTAINER_IMAGE"] ?? "testcontainers/sshd:1.1.0";
+export const REAPER_IMAGE =
+  process.env["RYUK_CONTAINER_IMAGE"] ?? "registry.testcontainers.com/testcontainers/ryuk:0.3.4";
+export const SSHD_IMAGE =
+  process.env["SSHD_CONTAINER_IMAGE"] ?? "registry.testcontainers.com/testcontainers/sshd:1.1.0";
