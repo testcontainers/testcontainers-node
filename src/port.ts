049a754f1ae0c847efad239ae89c37fea44e7751
Cristian Greco <cristianrgreco@gmail.com>
2023-03-06 08:27:25 +0000

    Lookup host IPs and use preferred host port binding according to host IP family

--- src/port.ts
+++ src/port.ts
@@ -1,3 +1,7 @@
+import * as net from "net";
+import { HostIps } from "./docker/lookup-host-ips";
+import { HostPortBindings } from "./docker/types";
+
 export type PortWithBinding = {
   container: number;
   host: number;
@@ -10,4 +14,14 @@ export const getContainerPort = (port: PortWithOptionalBinding): number =>
 
 export const hasHostBinding = (port: PortWithOptionalBinding): port is PortWithBinding => {
   return typeof port === "object" && port.host !== undefined;
+};
+
+export const resolveHostPortBinding = (hostIps: HostIps, hostPortBindings: HostPortBindings): number => {
+  for (const { family } of hostIps) {
+    const hostPortBinding = hostPortBindings.find(({ hostIp }) => net.isIP(hostIp) === family);
+    if (hostPortBinding !== undefined) {
+      return hostPortBinding.hostPort;
+    }
+  }
+  throw new Error("No host port found for host IP");
 };
