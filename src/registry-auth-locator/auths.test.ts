726057c5159af6c857be76798a0e0e90dd0a9ce5
Cristian Greco <cristianrgreco@gmail.com>
2023-03-07 13:30:59 +0000

    Do not return auth config when registry names partially match

--- src/registry-auth-locator/auths.test.ts
+++ src/registry-auth-locator/auths.test.ts
@@ -8,12 +8,12 @@ describe("Auths", () => {
   describe("getAuthConfig", () => {
     it("should return undefined when auths is undefined", async () => {
       const dockerConfig: DockerConfig = {};
-      expect(await locator.getAuthConfig("registry-name", dockerConfig)).toBe(undefined);
+      expect(await locator.getAuthConfig("registry-name", dockerConfig)).toBeUndefined();
     });
 
     it("should return undefined when auths does not contain registry name", async () => {
       const dockerConfig: DockerConfig = { auths: {} };
-      expect(await locator.getAuthConfig("registry-name", dockerConfig)).toBe(undefined);
+      expect(await locator.getAuthConfig("registry-name", dockerConfig)).toBeUndefined();
     });
 
     it("should return credentials from username and password", async () => {
@@ -33,6 +33,19 @@ describe("Auths", () => {
         registryAddress: "https://registry.example.com",
       };
       expect(await locator.getAuthConfig("https://registry.example.com", dockerConfig)).toEqual(authConfig);
+    });
+
+    it("should not return credentials for registry which is a partial match", async () => {
+      const dockerConfig: DockerConfig = {
+        auths: {
+          "https://registry.example.com": {
+            email: "user@example.com",
+            username: "user",
+            password: "pass",
+          },
+        },
+      };
+      expect(await locator.getAuthConfig("registry.example.co", dockerConfig)).toBeUndefined();
     });
 
     it("should return credentials from encoded auth", async () => {
