1e2b59369bd61f87d7f999a53a07054fa2ad9b74
Cristian Greco <cristianrgreco@gmail.com>
2023-03-07 14:58:33 +0000

    Remove hard-coded different IPv4/IPv6 port bindings

--- src/docker/functions/container/create-container.ts
+++ src/docker/functions/container/create-container.ts
@@ -5,7 +5,6 @@ import Dockerode, { PortMap as DockerodePortBindings } from "dockerode";
 import { getContainerPort, hasHostBinding, PortWithOptionalBinding } from "../../../port";
 import { createLabels } from "../create-labels";
 import { BindMount, Environment, ExtraHost, HealthCheck, Labels, TmpFs, Ulimits } from "../../types";
-import { RandomUniquePortGenerator } from "../../../port-generator";
 
 export type CreateContainerOptions = {
   imageName: DockerImageName;
@@ -53,7 +52,7 @@ export const createContainer = async (options: CreateContainerOptions): Promise<
         ExtraHosts: getExtraHosts(options.extraHosts),
         AutoRemove: options.autoRemove,
         NetworkMode: options.networkMode,
-        PortBindings: await getPortBindings(options.exposedPorts),
+        PortBindings: getPortBindings(options.exposedPorts),
         Binds: getBindMounts(options.bindMounts),
         Tmpfs: options.tmpFs,
         LogConfig: getLogConfig(options.useDefaultLogDriver),
@@ -91,17 +90,13 @@ const getExtraHosts = (extraHosts: ExtraHost[]): string[] => {
   return extraHosts.map((extraHost) => `${extraHost.host}:${extraHost.ipAddress}`);
 };
 
-const getPortBindings = async (exposedPorts: PortWithOptionalBinding[]): Promise<DockerodePortBindings> => {
+const getPortBindings = (exposedPorts: PortWithOptionalBinding[]): DockerodePortBindings => {
   const dockerodePortBindings: DockerodePortBindings = {};
-  const r = new RandomUniquePortGenerator();
   for (const exposedPort of exposedPorts) {
     if (hasHostBinding(exposedPort)) {
       dockerodePortBindings[exposedPort.container] = [{ HostPort: exposedPort.host.toString() }];
     } else {
-      dockerodePortBindings[exposedPort] = [
-        { HostIp: "0.0.0.0", HostPort: `${await r.generatePort()}` },
-        { HostIp: "::", HostPort: `${await r.generatePort()}` },
-      ];
+      dockerodePortBindings[exposedPort] = [{ HostPort: "0" }];
     }
   }
   return dockerodePortBindings;
