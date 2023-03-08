351973a08f7fe5ab6a211736dd0086c6f13a8c17
Cristian Greco <cristianrgreco@gmail.com>
2023-03-08 09:22:26 +0000

    Fixes for inconsistent IPv4/IPv6 host resolution (#485)

--- src/docker/lookup-host-ips.ts
+++ src/docker/lookup-host-ips.ts
@@ -5,7 +5,7 @@ export type HostIps = Array<{ address: string; family: number }>;
 
 export const lookupHostIps = async (host: string): Promise<HostIps> => {
   if (net.isIP(host) === 0) {
-    return await dns.promises.lookup(host, { all: true, verbatim: true });
+    return await dns.promises.lookup(host, { all: true });
   } else {
     return [{ address: host, family: net.isIP(host) }];
   }
