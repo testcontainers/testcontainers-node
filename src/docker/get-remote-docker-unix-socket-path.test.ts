119b9553d73e3f515b9badf94a43b8e9acd339da
Cristian Greco <cristianrgreco@gmail.com>
2023-03-10 08:40:42 +0000

    Support UNC for Windows socket path

--- src/docker/get-remote-docker-unix-socket-path.test.ts
+++ src/docker/get-remote-docker-unix-socket-path.test.ts
@@ -1,7 +1,7 @@
 import { getRemoteDockerUnixSocketPath } from "./get-remote-docker-unix-socket-path";
 
 test("should return TESTCONTAINERS_DOCKER_SOCKET_OVERRIDE when provided", () => {
-  const actual = getRemoteDockerUnixSocketPath("unix:///var/run/docker.sock", {
+  const actual = getRemoteDockerUnixSocketPath("unix:///var/run/docker.sock", "linux", {
     TESTCONTAINERS_DOCKER_SOCKET_OVERRIDE: "/var/run/another.sock",
   });
   expect(actual).toEqual("/var/run/another.sock");
@@ -9,12 +9,22 @@ test("should return TESTCONTAINERS_DOCKER_SOCKET_OVERRIDE when provided", () => 
 
 test("should return /var/run/docker.sock when URI is not a unix socket", () => {
   ["tcp://localhost:2375", "npipe:////./pipe/docker_engine"].forEach((uri) => {
-    const actual = getRemoteDockerUnixSocketPath(uri, {});
+    const actual = getRemoteDockerUnixSocketPath(uri, "linux", {});
     expect(actual).toEqual("/var/run/docker.sock");
   });
 });
 
 test("should return path of a unix socket", () => {
-  const actual = getRemoteDockerUnixSocketPath("unix:///var/run/docker.sock", {});
+  const actual = getRemoteDockerUnixSocketPath("unix:///var/run/docker.sock", "linux", {});
   expect(actual).toEqual("/var/run/docker.sock");
+});
+
+test("should return path of a unix socket", () => {
+  const actual = getRemoteDockerUnixSocketPath("unix:///var/run/docker.sock", "linux", {});
+  expect(actual).toEqual("/var/run/docker.sock");
+});
+
+test("should prepend / to follow the UNC for Windows", () => {
+  const actual = getRemoteDockerUnixSocketPath("unix:///var/run/docker.sock", "win32", {});
+  expect(actual).toEqual("//var/run/docker.sock");
 });
--- src/docker/get-remote-docker-unix-socket-path.ts
+++ src/docker/get-remote-docker-unix-socket-path.ts
@@ -1,11 +1,18 @@
-export const getRemoteDockerUnixSocketPath = (uri: string, env: NodeJS.ProcessEnv = process.env): string => {
+export const getRemoteDockerUnixSocketPath = (
+  uri: string,
+  platform: NodeJS.Platform = process.platform,
+  env: NodeJS.ProcessEnv = process.env
+): string => {
   if (env["TESTCONTAINERS_DOCKER_SOCKET_OVERRIDE"] !== undefined) {
     return env["TESTCONTAINERS_DOCKER_SOCKET_OVERRIDE"];
   }
 
+  let socketPath: string;
   if (uri.startsWith("unix://")) {
-    return uri.replace("unix://", "");
+    socketPath = uri.replace("unix://", "");
   } else {
-    return "/var/run/docker.sock";
+    socketPath = "/var/run/docker.sock";
   }
+
+  return platform === "win32" ? `/${socketPath}` : socketPath;
 };
