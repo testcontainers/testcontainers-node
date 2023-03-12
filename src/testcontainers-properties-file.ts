5db7a4f95f10bb5561c7ef7e1f1c66a16733c146
Cristian Greco <cristianrgreco@gmail.com>
2023-03-12 13:00:36 +0000

    Config strategy supports DOCKER_HOST

--- src/testcontainers-properties-file.ts
+++ src/testcontainers-properties-file.ts
@@ -10,32 +10,47 @@ let dockerHost: string | undefined;
 let dockerTlsVerify: string | undefined;
 let dockerCertPath: string | undefined;
 
+if (process.env["DOCKER_HOST"] !== undefined) {
+  dockerHost = process.env["DOCKER_HOST"];
+}
+if (process.env["DOCKER_TLS_VERIFY"] !== undefined) {
+  dockerTlsVerify = process.env["DOCKER_TLS_VERIFY"];
+}
+if (process.env["DOCKER_CERT_PATH"] !== undefined) {
+  dockerCertPath = process.env["DOCKER_CERT_PATH"];
+}
+
 if (existsSync(file)) {
   log.debug("Found .testcontainers.properties file");
   const string = readFileSync(file, { encoding: "utf-8" });
   const properties = propertiesReader("").read(string);
 
-  let logStr = "Loaded .testcontainers.properties file";
-
   const host = properties.get("docker.host") as string;
   if (host !== null) {
     dockerHost = host;
-    logStr += `, dockerHost: ${dockerHost}`;
   }
 
   const tlsVerify = properties.get("docker.tls.verify") as number;
   if (tlsVerify !== null) {
     dockerTlsVerify = `${tlsVerify}`;
-    logStr += `, dockerTlsVerify: ${dockerTlsVerify}`;
   }
 
   const certPath = properties.get("docker.cert.path") as string;
   if (certPath !== null) {
     dockerCertPath = certPath;
-    logStr += `, dockerCertPath: ${dockerCertPath}`;
   }
-
-  log.debug(logStr);
 }
+
+let logStr = "Loaded properties: ";
+if (dockerHost !== undefined) {
+  logStr += `dockerHost=${dockerHost}, `;
+}
+if (dockerTlsVerify !== undefined) {
+  logStr += `dockerTlsVerify=${dockerTlsVerify}, `;
+}
+if (dockerCertPath !== undefined) {
+  logStr += `dockerCertPath=${dockerCertPath}`;
+}
+log.debug(logStr);
 
 export { dockerHost, dockerTlsVerify, dockerCertPath };
