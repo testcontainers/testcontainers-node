f90c9781175f8e2404922d8cbeee9b96a4dba5f8
Cristian Greco <cristianrgreco@gmail.com>
2023-03-14 09:44:28 +0000

    Try default index server address

--- src/docker/functions/get-info.ts
+++ src/docker/functions/get-info.ts
@@ -1,4 +1,5 @@
 import Dockerode from "dockerode";
+import { log } from "../../logger";
 
 export type DockerInfo = {
   serverVersion: number;
@@ -12,6 +13,15 @@ export type DockerInfo = {
 
 export const getDockerInfo = async (dockerode: Dockerode): Promise<DockerInfo> => {
   const info = await dockerode.info();
+
+  let indexServerAddress: string;
+  if (info.IndexServerAddress === undefined || info.IndexServerAddress.length === 0) {
+    log.debug("Index server address is not set, using default");
+    indexServerAddress = "https://index.docker.io/v1/";
+  } else {
+    indexServerAddress = info.IndexServerAddress;
+  }
+
   return {
     serverVersion: info.ServerVersion,
     operatingSystem: info.OperatingSystem,
@@ -19,6 +29,6 @@ export const getDockerInfo = async (dockerode: Dockerode): Promise<DockerInfo> =
     architecture: info.Architecture,
     cpus: info.NCPU,
     memory: info.MemTotal,
-    indexServerAddress: info.IndexServerAddress ?? "https://index.docker.io/v1/",
+    indexServerAddress,
   };
 };
