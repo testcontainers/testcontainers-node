65f78343305b8abc30e96a04c13dbadc47f1080c
a.e.solovyev <a.e.solovyev@tinkoff.ru>
2023-03-16 18:17:55 +0500

    resources quota tests with single parameter only

--- src/generic-container/generic-container.test.ts
+++ src/generic-container/generic-container.test.ts
@@ -328,20 +328,6 @@ describe("GenericContainer", () => {
     await container.stop();
   });
 
-  it("should set resources quota", async () => {
-    const container = await new GenericContainer("cristianrgreco/testcontainer:1.1.14")
-      .withResourcesQuota({ memory: 0.5, cpu: 1 })
-      .start();
-
-    const dockerContainer = await getContainerById(container.getId());
-    const containerInfo = await dockerContainer.inspect();
-
-    expect(containerInfo.HostConfig.Memory).toEqual(536870912);
-    expect(containerInfo.HostConfig.NanoCpus).toEqual(1000000000);
-
-    await container.stop();
-  });
-
   it("should set privileged mode", async () => {
     const container = await new GenericContainer("cristianrgreco/testcontainer:1.1.14")
       .withPrivilegedMode()
@@ -569,6 +555,50 @@ describe("GenericContainer", () => {
     expect(output).toBe(content);
 
     await container.stop();
+  });
+
+  describe("resources quota", () => {
+    it("should set resources quota", async () => {
+      const container = await new GenericContainer("cristianrgreco/testcontainer:1.1.14")
+        .withResourcesQuota({ memory: 0.5, cpu: 1 })
+        .start();
+
+      const dockerContainer = await getContainerById(container.getId());
+      const containerInfo = await dockerContainer.inspect();
+
+      expect(containerInfo.HostConfig.Memory).toEqual(536870912);
+      expect(containerInfo.HostConfig.NanoCpus).toEqual(1000000000);
+
+      await container.stop();
+    });
+
+    it("should set resources quota memory only, cpu should be 0", async () => {
+      const container = await new GenericContainer("cristianrgreco/testcontainer:1.1.14")
+        .withResourcesQuota({ memory: 0.5 })
+        .start();
+
+      const dockerContainer = await getContainerById(container.getId());
+      const containerInfo = await dockerContainer.inspect();
+
+      expect(containerInfo.HostConfig.Memory).toEqual(536870912);
+      expect(containerInfo.HostConfig.NanoCpus).toEqual(0);
+
+      await container.stop();
+    });
+
+    it("should set resources quota cpu only, memory should be 0", async () => {
+      const container = await new GenericContainer("cristianrgreco/testcontainer:1.1.14")
+        .withResourcesQuota({ cpu: 1 })
+        .start();
+
+      const dockerContainer = await getContainerById(container.getId());
+      const containerInfo = await dockerContainer.inspect();
+
+      expect(containerInfo.HostConfig.Memory).toEqual(0);
+      expect(containerInfo.HostConfig.NanoCpus).toEqual(1000000000);
+
+      await container.stop();
+    });
   });
 
   describe("reuse", () => {
