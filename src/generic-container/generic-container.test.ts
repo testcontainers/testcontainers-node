8ed6aee76b26e36f0cefe9c456213f5d985c8bd9
a.e.solovyev <a.e.solovyev@tinkoff.ru>
2023-03-17 17:59:15 +0500

    docs note about tests docker features, fix tests for rootless docker

--- src/generic-container/generic-container.test.ts
+++ src/generic-container/generic-container.test.ts
@@ -558,19 +558,21 @@ describe("GenericContainer", () => {
   });
 
   describe("resources quota", () => {
-    it("should set resources quota", async () => {
-      const container = await new GenericContainer("cristianrgreco/testcontainer:1.1.14")
-        .withResourcesQuota({ memory: 0.5, cpu: 1 })
-        .start();
+    if (!process.env["CI_ROOTLESS"]) {
+      it("should set resources quota", async () => {
+        const container = await new GenericContainer("cristianrgreco/testcontainer:1.1.14")
+          .withResourcesQuota({ memory: 0.5, cpu: 1 })
+          .start();
 
-      const dockerContainer = await getContainerById(container.getId());
-      const containerInfo = await dockerContainer.inspect();
+        const dockerContainer = await getContainerById(container.getId());
+        const containerInfo = await dockerContainer.inspect();
 
-      expect(containerInfo.HostConfig.Memory).toEqual(536870912);
-      expect(containerInfo.HostConfig.NanoCpus).toEqual(1000000000);
+        expect(containerInfo.HostConfig.Memory).toEqual(536870912);
+        expect(containerInfo.HostConfig.NanoCpus).toEqual(1000000000);
 
-      await container.stop();
-    });
+        await container.stop();
+      });
+    }
 
     it("resources quota should be 0 for cpu and memory if not set by user", async () => {
       const container = await new GenericContainer("cristianrgreco/testcontainer:1.1.14").start();
@@ -598,19 +600,21 @@ describe("GenericContainer", () => {
       await container.stop();
     });
 
-    it("should set resources quota cpu only, memory should be 0", async () => {
-      const container = await new GenericContainer("cristianrgreco/testcontainer:1.1.14")
-        .withResourcesQuota({ cpu: 1 })
-        .start();
+    if (!process.env["CI_ROOTLESS"]) {
+      it("should set resources quota cpu only, memory should be 0", async () => {
+        const container = await new GenericContainer("cristianrgreco/testcontainer:1.1.14")
+          .withResourcesQuota({ cpu: 1 })
+          .start();
 
-      const dockerContainer = await getContainerById(container.getId());
-      const containerInfo = await dockerContainer.inspect();
+        const dockerContainer = await getContainerById(container.getId());
+        const containerInfo = await dockerContainer.inspect();
 
-      expect(containerInfo.HostConfig.Memory).toEqual(0);
-      expect(containerInfo.HostConfig.NanoCpus).toEqual(1000000000);
+        expect(containerInfo.HostConfig.Memory).toEqual(0);
+        expect(containerInfo.HostConfig.NanoCpus).toEqual(1000000000);
 
-      await container.stop();
-    });
+        await container.stop();
+      });
+    }
   });
 
   describe("reuse", () => {
