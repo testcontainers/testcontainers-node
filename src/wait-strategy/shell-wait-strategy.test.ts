2d4bae74ceccba60d6932fd3adf1a961ce0fffb4
Cristian Greco <cristianrgreco@gmail.com>
2023-04-05 14:12:09 +0100

    Fix shell wait strategy tests

--- src/wait-strategy/shell-wait-strategy.test.ts
+++ src/wait-strategy/shell-wait-strategy.test.ts
@@ -6,7 +6,7 @@ jest.setTimeout(180_000);
 describe("ShellWaitStrategy", () => {
   it("should wait for successful command", async () => {
     const container = await new GenericContainer("cristianrgreco/testcontainer:1.1.14")
-      .withCommand(["sh", "-c", "sleep 0.5; touch /tmp/test.lock; sleep infinity"])
+      .withCommand(["/bin/sh", "-c", "sleep 0.5; touch /tmp/test.lock; node index.js"])
       .withWaitStrategy(Wait.forSuccessfulCommand("stat /tmp/test.lock"))
       .start();
 
@@ -16,7 +16,6 @@ describe("ShellWaitStrategy", () => {
   it("should timeout when command not successful", async () => {
     await expect(() =>
       new GenericContainer("cristianrgreco/testcontainer:1.1.14")
-        .withCommand(["sh", "-c", "sleep infinity"])
         .withWaitStrategy(Wait.forSuccessfulCommand("stat /tmp/test.lock"))
         .withStartupTimeout(1000)
         .start()
