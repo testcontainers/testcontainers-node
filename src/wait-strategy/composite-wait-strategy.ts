fd76837332cfab8b78a889f489655dcf9635bf14
Cristian Greco <cristianrgreco@gmail.com>
2023-04-12 09:45:07 +0100

    CompositeWaitStrategy defaults to max startupTimeout

--- src/wait-strategy/composite-wait-strategy.ts
+++ src/wait-strategy/composite-wait-strategy.ts
@@ -5,6 +5,11 @@ import { BoundPorts } from "../bound-ports";
 export class CompositeWaitStrategy extends AbstractWaitStrategy {
   constructor(private readonly waitStrategies: WaitStrategy[]) {
     super();
+    this.withStartupTimeout(this.getMaxStartupTimeout());
+  }
+
+  private getMaxStartupTimeout(): number {
+    return Math.max(...this.waitStrategies.map((waitStrategy) => waitStrategy.getStartupTimeout()));
   }
 
   public async waitUntilReady(container: Dockerode.Container, boundPorts: BoundPorts, startTime?: Date): Promise<void> {
--- src/wait-strategy/wait-strategy.ts
+++ src/wait-strategy/wait-strategy.ts
@@ -5,6 +5,8 @@ export interface WaitStrategy {
   waitUntilReady(container: Dockerode.Container, boundPorts: BoundPorts, startTime?: Date): Promise<void>;
 
   withStartupTimeout(startupTimeout: number): WaitStrategy;
+
+  getStartupTimeout(): number;
 }
 
 export abstract class AbstractWaitStrategy implements WaitStrategy {
@@ -16,8 +18,12 @@ export abstract class AbstractWaitStrategy implements WaitStrategy {
     startTime?: Date
   ): Promise<void>;
 
-  public withStartupTimeout(startupTimeout: number): WaitStrategy {
+  public withStartupTimeout(startupTimeout: number): this {
     this.startupTimeout = startupTimeout;
     return this;
+  }
+
+  public getStartupTimeout(): number {
+    return this.startupTimeout;
   }
 }
