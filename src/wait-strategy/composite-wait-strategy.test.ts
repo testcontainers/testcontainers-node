524584ee4587e34e5e92ca2f9af885c61fb1d6b9
Cristian Greco <cristianrgreco@gmail.com>
2023-04-12 10:42:06 +0100

    CompositeWaitStrategy fixes

--- src/wait-strategy/composite-wait-strategy.test.ts
+++ src/wait-strategy/composite-wait-strategy.test.ts
@@ -3,7 +3,7 @@ import { AbstractWaitStrategy } from "./wait-strategy";
 import { BoundPorts } from "../bound-ports";
 import Dockerode from "dockerode";
 
-const fakeContainer = {} as Dockerode.Container;
+const fakeContainer = { id: "containerId" } as Dockerode.Container;
 const fakeBoundPorts = {} as BoundPorts;
 const fakeStartTime = {} as Date;
 
@@ -35,21 +35,34 @@ test("should set the startup timeout to the maximum of all wait strategies", asy
   expect(waitStrategy.getStartupTimeout()).toEqual(2000);
 });
 
-test("should override startup timeouts when once is provided", async () => {
-  const waitStrategy = new CompositeWaitStrategy([
-    new FakeWaitStrategy({ resolves: true }).withStartupTimeout(1000),
-    new FakeWaitStrategy({ resolves: true }).withStartupTimeout(2000),
-  ]).withStartupTimeout(3000);
+test("should override startup timeouts when one is provided", async () => {
+  const s1 = new FakeWaitStrategy({ resolves: true }).withStartupTimeout(1000);
+  const s2 = new FakeWaitStrategy({ resolves: true }).withStartupTimeout(2000);
+  const waitStrategy = new CompositeWaitStrategy([s1, s2]).withStartupTimeout(3000);
 
+  expect(s1.getStartupTimeout()).toEqual(3000);
+  expect(s2.getStartupTimeout()).toEqual(3000);
   expect(waitStrategy.getStartupTimeout()).toEqual(3000);
 });
 
+test("should enforce startup timeout", async () => {
+  const waitStrategy = new CompositeWaitStrategy([
+    new FakeWaitStrategy({ resolves: true }).withStartupTimeout(1000),
+    new FakeWaitStrategy({ resolves: true }).withStartupTimeout(1000),
+  ]).withStartupTimeout(0);
+
+  await expect(waitStrategy.waitUntilReady(fakeContainer, fakeBoundPorts, fakeStartTime)).rejects.toThrowError(
+    "not resolved after 0ms"
+  );
+});
+
 class FakeWaitStrategy extends AbstractWaitStrategy {
-  constructor(private opts: { resolves: boolean; error?: Error }) {
+  constructor(private opts: { resolves: boolean; error?: Error; delay?: number }) {
     super();
   }
 
-  waitUntilReady(): Promise<void> {
+  async waitUntilReady(): Promise<void> {
+    await new Promise((resolve) => setTimeout(resolve, this.opts.delay ?? 0));
     if (this.opts.resolves) {
       return Promise.resolve();
     } else {
--- src/wait-strategy/composite-wait-strategy.ts
+++ src/wait-strategy/composite-wait-strategy.ts
@@ -1,20 +1,38 @@
 import { AbstractWaitStrategy, WaitStrategy } from "./wait-strategy";
 import Dockerode from "dockerode";
 import { BoundPorts } from "../bound-ports";
+import { log } from "../logger";
 
 export class CompositeWaitStrategy extends AbstractWaitStrategy {
   constructor(private readonly waitStrategies: WaitStrategy[]) {
     super();
-    this.withStartupTimeout(this.getMaxStartupTimeout());
-  }
-
-  private getMaxStartupTimeout(): number {
-    return Math.max(...this.waitStrategies.map((waitStrategy) => waitStrategy.getStartupTimeout()));
+    this.withStartupTimeout(Math.max(...this.waitStrategies.map((waitStrategy) => waitStrategy.getStartupTimeout())));
   }
 
   public async waitUntilReady(container: Dockerode.Container, boundPorts: BoundPorts, startTime?: Date): Promise<void> {
-    await Promise.all(
-      this.waitStrategies.map((waitStrategy) => waitStrategy.waitUntilReady(container, boundPorts, startTime))
-    );
+    log.debug(`Starting composite wait strategy for ${container.id}`);
+
+    return new Promise((resolve, reject) => {
+      const onTimeout = () => {
+        const message = `Composite wait strategy not resolved after ${this.startupTimeout}ms for ${container.id}`;
+        log.error(message);
+        reject(new Error(message));
+      };
+
+      const timeout = setTimeout(onTimeout, this.startupTimeout);
+
+      Promise.all(
+        this.waitStrategies.map((waitStrategy) => waitStrategy.waitUntilReady(container, boundPorts, startTime))
+      )
+        .then(() => resolve())
+        .catch((err) => reject(err))
+        .finally(() => clearTimeout(timeout));
+    });
+  }
+
+  public override withStartupTimeout(startupTimeout: number): this {
+    super.withStartupTimeout(startupTimeout);
+    this.waitStrategies.forEach((waitStrategy) => waitStrategy.withStartupTimeout(startupTimeout));
+    return this;
   }
 }
