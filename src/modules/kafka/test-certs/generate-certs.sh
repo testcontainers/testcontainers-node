92546092f72dc77113e771917b36b44a8f980adb
Cristian Greco <cristianrgreco@gmail.com>
2023-04-24 14:21:11 +0100

    Renew certificates used by Kafka tests (#542)

--- src/modules/kafka/test-certs/generate-certs.sh
+++ src/modules/kafka/test-certs/generate-certs.sh
@@ -1,8 +1,8 @@
 #generate server cert
-keytool -keystore /var/output/kafka.server.keystore.pfx -storetype PKCS12 -alias localhost -keyalg RSA -validity 365 -genkey -storepass serverKeystorePassword -dname CN=localhost -ext SAN=DNS:localhost
+keytool -keystore /var/output/kafka.server.keystore.pfx -storetype PKCS12 -alias localhost -keyalg RSA -validity 36500 -genkey -storepass serverKeystorePassword -dname CN=localhost -ext SAN=DNS:localhost
 
 #create a certificate authority (CA)
-openssl req -new -x509 -keyout /var/output/ca-key -out /var/output/ca-cert -days 365 -subj '/CN=myCA' -passin pass:password  -passout pass:password
+openssl req -new -x509 -keyout /var/output/ca-key -out /var/output/ca-cert -days 36500 -subj '/CN=myCA' -passin pass:password  -passout pass:password
 
 #import CA cert so that it is trusted
 keytool -keystore /var/output/kafka.client.truststore.pfx -storetype PKCS12 -alias CARoot -importcert -noprompt -file /var/output/ca-cert -storepass clientTruststorePassword
@@ -10,7 +10,7 @@ keytool -keystore /var/output/kafka.server.truststore.pfx -storetype PKCS12 -ali
 
 #sign the server certificate
 keytool -keystore /var/output/kafka.server.keystore.pfx -storetype PKCS12 -alias localhost -certreq -file /var/output/cert-file -storepass serverKeystorePassword
-openssl x509 -req -CA /var/output/ca-cert -CAkey /var/output/ca-key -in /var/output/cert-file -out /var/output/cert-signed -days 365 -CAcreateserial -passin pass:password
+openssl x509 -req -CA /var/output/ca-cert -CAkey /var/output/ca-key -in /var/output/cert-file -out /var/output/cert-signed -days 36500 -CAcreateserial -passin pass:password
 keytool -keystore /var/output/kafka.server.keystore.pfx -storetype PKCS12 -alias CARoot -importcert -noprompt -file /var/output/ca-cert -storepass serverKeystorePassword
 keytool -keystore /var/output/kafka.server.keystore.pfx -storetype PKCS12 -alias localhost -importcert -noprompt -file /var/output/cert-signed -storepass serverKeystorePassword
 
