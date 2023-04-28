fb91a4a221d5f7b423dc94967fbf3dc19a505994
Cristian Greco <cristianrgreco@gmail.com>
2023-04-28 14:30:31 +0100

    Update ryuk 0.3.4->0.4.0 (#552)

--- src/docker/functions/create-labels.test.ts
+++ src/docker/functions/create-labels.test.ts
@@ -1,6 +1,4 @@
 import { createLabels } from "./create-labels";
-import { DockerImageName } from "../../docker-image-name";
-import { REAPER_IMAGE } from "../../images";
 import { version } from "../../../package.json";
 import {
   LABEL_TESTCONTAINERS,
@@ -25,11 +23,6 @@ test("should add session ID label when not reusable (for the Reaper)", () => {
 
 test("should not add session ID label when reusable (to avoid Reaper)", () => {
   const sessionId = createLabels(true)[LABEL_TESTCONTAINERS_SESSION_ID];
-  expect(sessionId).toBeUndefined();
-});
-
-test("should not add session ID label when the container is the Reaper (to avoid Reaper killing self)", () => {
-  const sessionId = createLabels(false, DockerImageName.fromString(REAPER_IMAGE))[LABEL_TESTCONTAINERS_SESSION_ID];
   expect(sessionId).toBeUndefined();
 });
 
