31c698f54f0ca652b90e506a9d9f72b590f4dc00
Cristian Greco <cristianrgreco@gmail.com>
2023-05-23 15:11:13 +0100

    Test

--- src/logger.ts
+++ src/logger.ts
@@ -60,3 +60,4 @@ export const composeLog = new Logger("testcontainers:compose", false);
 export const buildLog = new Logger("testcontainers:build", false);
 export const pullLog = new Logger("testcontainers:pull", false);
 export const execLog = new Logger("testcontainers:exec", false);
+export const test = new Logger("testcontainers:test", false);
