1b9620f7488a5ad413bd28940c19705ebb7f9639
Andreas Schujkow <andreas.schujkow@hivemq.com>
2023-07-11 11:06:02 +0200

    test(modules): add test proposal
    https://github.com/testcontainers/testcontainers-node/pull/598#discussion_r1259383345

--- src/modules/hivemq/hivemq-container.test.ts
+++ src/modules/hivemq/hivemq-container.test.ts
@@ -5,53 +5,24 @@ describe("HiveMQContainer", () => {
   jest.setTimeout(240_000);
 
   // connect {
-  it("should connect to HiveMQ Community Edition via MQTT.js", async () => {
-    const TEST_TOPIC = "test/topic";
-    const TEST_MESSAGE = "Test_Message";
+  it("should connect to HiveMQ Community Edition via MQTT.js", (done) => {
+    new HiveMQContainer().start().then((hiveMQContainer) => {
+      const testMqttClient = mqtt.connect(hiveMQContainer.getConnectionString());
 
-    const hiveMQContainer = await new HiveMQContainer().start();
-
-    const testMqttClient = mqtt.connect(hiveMQContainer.getConnectionString());
-
-    async function onConnect() {
-      return new Promise((resolve) => {
-        testMqttClient.on("connect", () => {
-          resolve(undefined);
-        });
-      });
-    }
-
-    async function onSubscribe() {
-      return new Promise((resolve, reject) => {
-        testMqttClient.subscribe(TEST_TOPIC, (error) => {
-          if (error) {
-            reject();
-          }
-
-          resolve(undefined);
-        });
-      });
-    }
-
-    await onConnect();
-    await onSubscribe();
-
-    const messagePromise = new Promise<string>((resolve) => {
       testMqttClient.on("message", (topic, message) => {
-        resolve(message.toString());
+        expect(message.toString()).toEqual("Test Message");
+        testMqttClient.end();
+        done();
+      });
+
+      testMqttClient.on("connect", () => {
+        testMqttClient.subscribe("test", (error) => {
+          if (!error) {
+            testMqttClient.publish("test", "Test Message");
+          }
+        });
       });
     });
-
-    const publishPromise = new Promise((resolve) => {
-      testMqttClient.publish(TEST_TOPIC, TEST_MESSAGE);
-      resolve(undefined);
-    });
-
-    const [messageResponse] = await Promise.all([messagePromise, publishPromise]);
-
-    expect(messageResponse).toEqual(TEST_MESSAGE);
-
-    testMqttClient.end();
   });
   // }
 });
