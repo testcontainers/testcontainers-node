e076ba9ba3896e635eab8f46e803c3f55546a3c6
Cristian Greco <cristianrgreco@gmail.com>
2023-08-23 10:31:54 +0100

    Run each test in separate process

--- packages/testcontainers/smoke-tests/tcc-turbo-mode.mjs
+++ packages/testcontainers/smoke-tests/tcc-turbo-mode.mjs
@@ -1,11 +1,8 @@
 import { GenericContainer } from "../build/index.js";
 
-const MAX_CONCURRENCY = 4;
+const container = await new GenericContainer("alpine:3.12")
+  .withName("there_can_only_be_one")
+  .withCommand(["sleep", "infinity"])
+  .start();
 
-const containers = await Promise.all(Array(MAX_CONCURRENCY).fill(0).map((_, i) =>
-  new GenericContainer("alpine:3.12")
-    .withName("there_can_only_be_one")
-    .withCommand(["sleep", "infinity"])
-    .start()));
-
-await Promise.all(containers.map(container => container.stop()));
+await container.stop();
