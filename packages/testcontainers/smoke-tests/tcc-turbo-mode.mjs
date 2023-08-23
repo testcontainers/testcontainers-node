b4e37800ad429c5ba6011c4a7ed91495a3a27272
Cristian Greco <cristianrgreco@gmail.com>
2023-08-23 10:16:35 +0100

    Add TCC smoke test to CI pipeline

---
+++ packages/testcontainers/smoke-tests/tcc-turbo-mode.mjs
@@ -0,0 +1,12 @@
+import { GenericContainer } from "./build/index.js";
+
+const MAX_CONCURRENCY = 4;
+
+const containers = await Promise.all(Array(MAX_CONCURRENCY).fill(0).map((_, i) => {
+  return new GenericContainer("alpine:3.12")
+    .withName("there_can_only_be_one")
+    .withCommand(["sleep", "infinity"])
+    .start();
+}));
+
+await Promise.all(containers.map(container => container.stop()));
