9c966a51f141fad014481f2dd880c997d51c1137
Cristian Greco <cristianrgreco@gmail.com>
2023-08-23 10:22:47 +0100

    Show logs

--- packages/testcontainers/smoke-tests/tcc-turbo-mode.mjs
+++ packages/testcontainers/smoke-tests/tcc-turbo-mode.mjs
@@ -2,11 +2,10 @@ import { GenericContainer } from "../build/index.js";
 
 const MAX_CONCURRENCY = 4;
 
-const containers = await Promise.all(Array(MAX_CONCURRENCY).fill(0).map((_, i) => {
-  return new GenericContainer("alpine:3.12")
+const containers = await Promise.all(Array(MAX_CONCURRENCY).fill(0).map((_, i) =>
+  new GenericContainer("alpine:3.12")
     .withName("there_can_only_be_one")
     .withCommand(["sleep", "infinity"])
-    .start();
-}));
+    .start()));
 
 await Promise.all(containers.map(container => container.stop()));
