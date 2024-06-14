728b7e37ec50b953fa719fbf67408d9b3c468de5
Ignasi <ignasi.lopez.luna@gmail.com>
2024-06-14 15:06:47 +0200

    Add GPU capability

--- packages/modules/ollama/src/ollama-container.ts
+++ packages/modules/ollama/src/ollama-container.ts
@@ -1,4 +1,10 @@
-import { AbstractStartedContainer, GenericContainer, StartedTestContainer, Wait } from "testcontainers";
+import {
+  AbstractStartedContainer,
+  GenericContainer,
+  getContainerRuntimeClient,
+  StartedTestContainer,
+  Wait,
+} from "testcontainers";
 
 export const OLLAMA_PORT = 11434;
 
@@ -8,6 +14,19 @@ export class OllamaContainer extends GenericContainer {
     this.withExposedPorts(OLLAMA_PORT)
       .withWaitStrategy(Wait.forLogMessage("Listening on "))
       .withStartupTimeout(120_000);
+
+    getContainerRuntimeClient().then((client) => {
+      const runtimes = client.info.containerRuntime.runtimes;
+      if (runtimes.includes("nvidia")) {
+        this.hostConfig.DeviceRequests = [
+          {
+            Driver: "nvidia",
+            Count: -1,
+            Capabilities: [["gpu"]],
+          },
+        ];
+      }
+    });
   }
 
   public override async start(): Promise<StartedOllamaContainer> {
