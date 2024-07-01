128e713c8adf59cda78339912d198c2ff4fea1e8
Tommi Finnilä <tommi.finnila@alfame.com>
2024-07-01 11:39:18 +0300

    Fix types for ollama-container's test

--- packages/modules/ollama/src/ollama-container.test.ts
+++ packages/modules/ollama/src/ollama-container.test.ts
@@ -7,7 +7,7 @@ describe("OllamaContainer", () => {
     const container = await new OllamaContainer("ollama/ollama:0.1.44").start();
     const response = await fetch(`${container.getEndpoint()}/api/version`);
     expect(response.status).toEqual(200);
-    const body = await response.json();
+    const body = (await response.json()) as { version: string };
     expect(body.version).toEqual("0.1.44");
     await container.stop();
   });
@@ -18,7 +18,7 @@ describe("OllamaContainer", () => {
     console.log(execResult.output);
     const response = await fetch(`${container.getEndpoint()}/api/tags`);
     expect(response.status).toEqual(200);
-    const body = await response.json();
+    const body = (await response.json()) as { models: { name: string }[] };
     expect(body.models[0].name).toContain("all-minilm");
 
     const newImageName: string = "tc-ollama-allminilm-" + (Math.random() + 1).toString(36).substring(4).toLowerCase();
@@ -27,7 +27,7 @@ describe("OllamaContainer", () => {
     const newContainer = await new OllamaContainer(newImageName).start();
     const response2 = await fetch(`${newContainer.getEndpoint()}/api/tags`);
     expect(response2.status).toEqual(200);
-    const body2 = await response2.json();
+    const body2 = (await response2.json()) as { models: { name: string }[] };
     expect(body2.models[0].name).toContain("all-minilm");
     await container.stop();
     await newContainer.stop();
