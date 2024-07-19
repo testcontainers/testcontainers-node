f498b850c04ad05d04ae49db0dab7f5e00bf0570
Cristian Greco <cristianrgreco@gmail.com>
2024-07-19 09:48:25 +0100

    Add github-actions reporter to Jest

--- jest.config.ts
+++ jest.config.ts
@@ -2,6 +2,7 @@ import type { Config } from "jest";
 
 const config: Config = {
   projects: ["<rootDir>/packages/testcontainers/jest.config.ts", "<rootDir>/packages/modules/*/jest.config.ts"],
+  reporters: [["github-actions", { silent: false }], "summary"],
 };
 
 export default config;
