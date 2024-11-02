1930c367d858d9c535a988fd09b97374c8bc8368
Sampo Silvennoinen <20028934+stscoundrel@users.noreply.github.com>
2024-11-02 12:12:40 +0200

    Add MariaDB module (#851)

---
+++ packages/modules/mariadb/src/mariadb-container.test.ts
@@ -0,0 +1,128 @@
+import mariadb from "mariadb";
+import { MariaDbContainer } from "./mariadb-container";
+
+describe("MariaDb", () => {
+  jest.setTimeout(240_000);
+
+  // connect {
+  it("should connect and execute query", async () => {
+    const container = await new MariaDbContainer().start();
+
+    const client = await mariadb.createConnection({
+      host: container.getHost(),
+      port: container.getPort(),
+      database: container.getDatabase(),
+      user: container.getUsername(),
+      password: container.getUserPassword(),
+    });
+
+    const rows = await client.query("SELECT 1 as res");
+    expect(rows).toEqual([{ res: 1 }]);
+
+    await client.end();
+    await container.stop();
+  });
+  // }
+
+  // uriConnect {
+  it("should work with database URI", async () => {
+    const username = "testUser";
+    const password = "testPassword";
+    const database = "testDB";
+
+    // Test non-root user
+    const container = await new MariaDbContainer()
+      .withUsername(username)
+      .withUserPassword(password)
+      .withDatabase(database)
+      .start();
+    expect(container.getConnectionUri()).toEqual(
+      `mariadb://${username}:${password}@${container.getHost()}:${container.getPort()}/${database}`
+    );
+    await container.stop();
+
+    // Test root user
+    const rootContainer = await new MariaDbContainer().withRootPassword(password).withDatabase(database).start();
+    expect(rootContainer.getConnectionUri(true)).toEqual(
+      `mariadb://root:${password}@${rootContainer.getHost()}:${rootContainer.getPort()}/${database}`
+    );
+    await rootContainer.stop();
+  });
+  // }
+
+  // setDatabase {
+  it("should set database", async () => {
+    const container = await new MariaDbContainer().withDatabase("customDatabase").start();
+
+    const client = await mariadb.createConnection({
+      host: container.getHost(),
+      port: container.getPort(),
+      database: container.getDatabase(),
+      user: container.getUsername(),
+      password: container.getUserPassword(),
+    });
+
+    const rows = await client.query("SELECT DATABASE() as res");
+    expect(rows).toEqual([{ res: "customDatabase" }]);
+
+    await client.end();
+    await container.stop();
+  });
+  // }
+
+  // setUsername {
+  it("should set username", async () => {
+    const container = await new MariaDbContainer().withUsername("customUsername").start();
+
+    const client = await mariadb.createConnection({
+      host: container.getHost(),
+      port: container.getPort(),
+      database: container.getDatabase(),
+      user: container.getUsername(),
+      password: container.getUserPassword(),
+    });
+
+    const rows = await client.query("SELECT CURRENT_USER() as res");
+    expect(rows).toEqual([{ res: "customUsername@%" }]);
+
+    await client.end();
+    await container.stop();
+  });
+  // }
+
+  // insertAndFetchData {
+  it("should create a table, insert a row, and fetch that row", async () => {
+    const container = await new MariaDbContainer().start();
+
+    const client = await mariadb.createConnection({
+      host: container.getHost(),
+      port: container.getPort(),
+      database: container.getDatabase(),
+      user: container.getUsername(),
+      password: container.getUserPassword(),
+    });
+
+    // Create table
+    await client.query(`
+        CREATE TABLE users (
+          id INT AUTO_INCREMENT PRIMARY KEY,
+          name VARCHAR(255) NOT NULL,
+          email VARCHAR(255) NOT NULL UNIQUE
+        );
+      `);
+
+    // Insert a row
+    const name = "John Doe";
+    const email = "john.doe@example.com";
+    const insertResult = await client.query("INSERT INTO users (name, email) VALUES (?, ?)", [name, email]);
+    expect(insertResult.affectedRows).toBe(1);
+
+    // Fetch the row
+    const [user] = await client.query("SELECT id, name, email FROM users WHERE email = ?", [email]);
+    expect(user).toEqual({ id: expect.any(Number), name, email });
+
+    await client.end();
+    await container.stop();
+  });
+  // }
+});
---
+++ packages/modules/mariadb/src/mariadb-container.ts
@@ -0,0 +1,96 @@
+import { AbstractStartedContainer, GenericContainer, StartedTestContainer } from "testcontainers";
+
+const MARIADB_PORT = 3306;
+
+export class MariaDbContainer extends GenericContainer {
+  private database = "test";
+  private username = "test";
+  private userPassword = "test";
+  private rootPassword = "test";
+
+  constructor(image = "mariadb:11.5.2") {
+    super(image);
+    this.withExposedPorts(MARIADB_PORT).withStartupTimeout(120_000);
+  }
+
+  public withDatabase(database: string): this {
+    this.database = database;
+    return this;
+  }
+
+  public withUsername(username: string): this {
+    this.username = username;
+    return this;
+  }
+
+  public withRootPassword(rootPassword: string): this {
+    this.rootPassword = rootPassword;
+    return this;
+  }
+
+  public withUserPassword(userPassword: string): this {
+    this.userPassword = userPassword;
+    return this;
+  }
+
+  public override async start(): Promise<StartedMariaDbContainer> {
+    this.withEnvironment({
+      MARIADB_DATABASE: this.database,
+      MARIADB_ROOT_PASSWORD: this.rootPassword,
+      MARIADB_USER: this.username,
+      MARIADB_PASSWORD: this.userPassword,
+    });
+    return new StartedMariaDbContainer(
+      await super.start(),
+      this.database,
+      this.username,
+      this.userPassword,
+      this.rootPassword
+    );
+  }
+}
+
+export class StartedMariaDbContainer extends AbstractStartedContainer {
+  private readonly port: number;
+
+  constructor(
+    startedTestContainer: StartedTestContainer,
+    private readonly database: string,
+    private readonly username: string,
+    private readonly userPassword: string,
+    private readonly rootPassword: string
+  ) {
+    super(startedTestContainer);
+    this.port = startedTestContainer.getMappedPort(MARIADB_PORT);
+  }
+
+  public getPort(): number {
+    return this.port;
+  }
+
+  public getDatabase(): string {
+    return this.database;
+  }
+
+  public getUsername(): string {
+    return this.username;
+  }
+
+  public getUserPassword(): string {
+    return this.userPassword;
+  }
+
+  public getRootPassword(): string {
+    return this.rootPassword;
+  }
+
+  public getConnectionUri(isRoot = false): string {
+    const url = new URL("", "mariadb://");
+    url.hostname = this.getHost();
+    url.port = this.getPort().toString();
+    url.pathname = this.getDatabase();
+    url.username = isRoot ? "root" : this.getUsername();
+    url.password = isRoot ? this.getRootPassword() : this.getUserPassword();
+    return url.toString();
+  }
+}
