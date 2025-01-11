61224197655e7f225d2398e1cb6663eff8d51540
Victor <victor.mendele68@gmail.com>
2025-01-11 20:43:01 +0100

    Add Mockserver module (#892)

---
+++ packages/modules/mockserver/src/mockserver-container.test.ts
@@ -0,0 +1,33 @@
+import { mockServerClient } from "mockserver-client";
+import superagent from "superagent";
+import { MockserverContainer } from "./mockserver-container";
+
+describe.only("MockserverContainer", () => {
+  jest.setTimeout(240_000);
+
+  // startContainer {
+  it("should start and accept mocks", async () => {
+    const container = await new MockserverContainer().start();
+    const client = mockServerClient(container.getHost(), container.getMockserverPort());
+    const url = container.getUrl();
+
+    await client.mockAnyResponse({
+      httpRequest: {
+        method: "GET",
+        path: "/foo",
+      },
+      httpResponse: {
+        body: {
+          string: "bar",
+        },
+        statusCode: 200,
+      },
+    });
+
+    const response = await superagent.get(`${url}/foo`);
+
+    expect(response.statusCode).toBe(200);
+    expect(response.text).toBe("bar");
+  });
+  // }
+});
