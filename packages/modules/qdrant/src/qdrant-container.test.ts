b6faf5da8756c9c0a27fc3f0cf738bf663ad7e44
Anush <anushshetty90@gmail.com>
2025-03-06 13:51:45 +0530

    Update docs to demonstrate specifying Qdrant version (#917)

--- packages/modules/qdrant/src/qdrant-container.test.ts
+++ packages/modules/qdrant/src/qdrant-container.test.ts
@@ -8,7 +8,7 @@ describe("QdrantContainer", () => {
 
   // connectQdrantSimple {
   it("should connect to the client", async () => {
-    const container = await new QdrantContainer().start();
+    const container = await new QdrantContainer("qdrant/qdrant:v1.13.4").start();
 
     const client = new QdrantClient({ url: `http://${container.getRestHostAddress()}` });
 
@@ -22,7 +22,7 @@ describe("QdrantContainer", () => {
   it("should work with valid API keys", async () => {
     const apiKey = crypto.randomUUID();
 
-    const container = await new QdrantContainer().withApiKey(apiKey).start();
+    const container = await new QdrantContainer("qdrant/qdrant:v1.13.4").withApiKey(apiKey).start();
 
     const client = new QdrantClient({ url: `http://${container.getRestHostAddress()}`, apiKey });
 
@@ -35,21 +35,23 @@ describe("QdrantContainer", () => {
   it("should fail for invalid API keys", async () => {
     const apiKey = crypto.randomUUID();
 
-    const container = await new QdrantContainer().withApiKey(apiKey).start();
+    const container = await new QdrantContainer("qdrant/qdrant:v1.13.4").withApiKey(apiKey).start();
 
     const client = new QdrantClient({
       url: `http://${container.getRestHostAddress()}`,
       apiKey: "INVALID_KEY_" + crypto.randomUUID(),
     });
 
-    expect(client.getCollections()).rejects.toThrow("Forbidden");
+    expect(client.getCollections()).rejects.toThrow("Unauthorized");
 
     await container.stop();
   });
 
   // connectQdrantWithConfig {
   it("should work with config files - valid API key", async () => {
-    const container = await new QdrantContainer().withConfigFile(path.resolve(__dirname, "test_config.yaml")).start();
+    const container = await new QdrantContainer("qdrant/qdrant:v1.13.4")
+      .withConfigFile(path.resolve(__dirname, "test_config.yaml"))
+      .start();
 
     const client = new QdrantClient({ url: `http://${container.getRestHostAddress()}`, apiKey: "SOME_TEST_KEY" });
 
@@ -60,14 +62,16 @@ describe("QdrantContainer", () => {
   // }
 
   it("should work with config files - invalid API key", async () => {
-    const container = await new QdrantContainer().withConfigFile(path.resolve(__dirname, "test_config.yaml")).start();
+    const container = await new QdrantContainer("qdrant/qdrant:v1.13.4")
+      .withConfigFile(path.resolve(__dirname, "test_config.yaml"))
+      .start();
 
     const client = new QdrantClient({
       url: `http://${container.getRestHostAddress()}`,
       apiKey: "INVALID_KEY_" + crypto.randomUUID(),
     });
 
-    expect(client.getCollections()).rejects.toThrow("Forbidden");
+    expect(client.getCollections()).rejects.toThrow("Unauthorized");
 
     await container.stop();
   });
