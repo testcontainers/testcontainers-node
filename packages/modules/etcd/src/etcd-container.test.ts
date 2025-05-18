0120446cec707cd12856c72e1f87f19f46dcc169
prenaissance <alex.andries.aa@gmail.com>
2025-05-18 20:53:25 +0300

    docs: add etcd to mkdocs

--- packages/modules/etcd/src/etcd-container.test.ts
+++ packages/modules/etcd/src/etcd-container.test.ts
@@ -1,11 +1,17 @@
 import { Etcd3 } from "etcd3";
-import { EtcdContainer } from "./etcd-container";
+import { promisify } from "node:util";
+import { EtcdContainer, StartedEtcdContainer } from "./etcd-container";
+
+const sleep = promisify(setTimeout);
 
 describe("etcd", () => {
   it("should construct a container", async () => {
-    await new EtcdContainer().start();
+    const container = await new EtcdContainer().start();
+    expect(container).toBeInstanceOf(StartedEtcdContainer);
+    container.stop();
   });
 
+  // readWrite {
   it("should connect and perform read/write operations", async () => {
     const container = await new EtcdContainer().start();
     const client = new Etcd3({
@@ -17,5 +23,27 @@ describe("etcd", () => {
     await client.put(key).value(value);
     const result = await client.get(key).string();
     expect(result).toEqual(value);
+
+    await container.stop();
   });
+  // }
+
+  // subscribe {
+  it("should subscribe to key changes", async () => {
+    const subscriber = vi.fn();
+    const container = await new EtcdContainer().start();
+    const client = new Etcd3({
+      hosts: container.getClientEndpoint(),
+    });
+    const key = "foo";
+    const value = "bar";
+    const watcher = await client.watch().key(key).create();
+    watcher.on("put", subscriber);
+    await client.put(key).value(value);
+    await sleep(1000);
+    expect(subscriber).toHaveBeenCalled();
+    await watcher.cancel();
+    await container.stop();
+  });
+  // }
 });
