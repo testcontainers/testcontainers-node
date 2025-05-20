dd559619776bf77d3763fc1f41b3112eeb9ddf29
Carlo Cervellin <carlo.cervellin@nearform.com>
2025-05-20 18:06:59 +0200

    test: update

--- packages/modules/redis/src/redis-container.test.ts
+++ packages/modules/redis/src/redis-container.test.ts
@@ -107,9 +107,9 @@ describe("RedisContainer", { timeout: 240_000 }, () => {
     const container = await new RedisContainer("redis/redis-stack-server:latest").withPassword("testPassword").start();
     const client = await connectTo(container);
 
-    await client.json.set("key", "$", JSON.stringify({ name: "test" }));
+    await client.json.set("key", "$", { name: "test" });
     const result = await client.json.get("key");
-    expect(result).toEqual(JSON.stringify({ name: "test" }));
+    expect(result).toEqual({ name: "test" });
 
     await client.disconnect();
     await container.stop();
