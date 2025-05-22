359914f327ff384e4d73ad331a0bd76489020879
Jeremy Dinsel <jeremy.dinsel@xealth.io>
2025-05-22 18:44:15 +0000

    restore container version -- tests still pass

--- packages/modules/nats/src/nats-container.ts
+++ packages/modules/nats/src/nats-container.ts
@@ -11,7 +11,7 @@ export class NatsContainer extends GenericContainer {
   private args = new Set<string>();
   private values = new Map<string, string | undefined>();
 
-  constructor(image = "nats:2.11.3-alpine") {
+  constructor(image = "nats:2.8.4-alpine") {
     super(image);
 
     this.withUsername("test");
