da96ae0236b850b7fe04528ca16a6f31899c5328
Cristian Greco <cristianrgreco@gmail.com>
2025-05-28 12:24:54 +0100

    Fix Elasticsearch tests

--- packages/modules/elasticsearch/src/elasticsearch-container.test.ts
+++ packages/modules/elasticsearch/src/elasticsearch-container.test.ts
@@ -3,12 +3,16 @@ import { getImage } from "../../../testcontainers/src/utils/test-helper";
 import { ElasticsearchContainer } from "./elasticsearch-container";
 
 const IMAGE = getImage(__dirname);
+const images = ["elasticsearch:7.17.28", "elasticsearch:8.18.1", IMAGE];
 
 describe("ElasticsearchContainer", { timeout: 180_000 }, () => {
   // createIndex {
-  it("should create an index", async () => {
-    const container = await new ElasticsearchContainer(IMAGE).start();
-    const client = new Client({ node: container.getHttpUrl() });
+  it.each(images)("should create an index with %s", async (image) => {
+    const container = await new ElasticsearchContainer(image).start();
+    const client = new Client({
+      node: container.getHttpUrl(),
+      auth: { username: container.getUsername(), password: container.getPassword() },
+    });
 
     await client.indices.create({ index: "people" });
 
@@ -20,7 +24,10 @@ describe("ElasticsearchContainer", { timeout: 180_000 }, () => {
   // indexDocument {
   it("should index a document", async () => {
     const container = await new ElasticsearchContainer(IMAGE).start();
-    const client = new Client({ node: container.getHttpUrl() });
+    const client = new Client({
+      node: container.getHttpUrl(),
+      auth: { username: container.getUsername(), password: container.getPassword() },
+    });
 
     const document = {
       id: "1",
@@ -41,7 +48,24 @@ describe("ElasticsearchContainer", { timeout: 180_000 }, () => {
     const container = await new ElasticsearchContainer(IMAGE).start();
     await container.restart();
 
-    const client = new Client({ node: container.getHttpUrl() });
+    const client = new Client({
+      node: container.getHttpUrl(),
+      auth: { username: container.getUsername(), password: container.getPassword() },
+    });
+
+    await client.indices.create({ index: "people" });
+
+    expect(await client.indices.exists({ index: "people" })).toBe(true);
+    await container.stop();
+  }); // }
+
+  it("should set custom password", async () => {
+    const container = await new ElasticsearchContainer(IMAGE).withPassword("testPassword").start();
+
+    const client = new Client({
+      node: container.getHttpUrl(),
+      auth: { username: container.getUsername(), password: container.getPassword() },
+    });
 
     await client.indices.create({ index: "people" });
 
