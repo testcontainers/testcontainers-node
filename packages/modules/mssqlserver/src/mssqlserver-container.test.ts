d366dbc19c437b86ad7a827ce022d8129b7cf74f
Cristian Greco <cristianrgreco@gmail.com>
2025-07-28 13:41:34 +0100

    Mssqlserver

--- packages/modules/mssqlserver/src/mssqlserver-container.test.ts
+++ packages/modules/mssqlserver/src/mssqlserver-container.test.ts
@@ -5,8 +5,8 @@ import { MSSQLServerContainer } from "./mssqlserver-container";
 const IMAGE = getImage(__dirname);
 
 describe("MSSqlServerContainer", { timeout: 180_000 }, () => {
-  // connect {
   it("should connect and return a query result", async () => {
+    // mssqlConnect {
     await using container = await new MSSQLServerContainer(IMAGE).acceptLicense().start();
 
     const sqlConfig: config = {
@@ -15,27 +15,20 @@ describe("MSSqlServerContainer", { timeout: 180_000 }, () => {
       database: container.getDatabase(),
       server: container.getHost(),
       port: container.getPort(),
-      pool: {
-        max: 1,
-        min: 0,
-        idleTimeoutMillis: 30000,
-      },
-      options: {
-        trustServerCertificate: true,
-      },
+      pool: { max: 1, min: 0, idleTimeoutMillis: 30000 },
+      options: { trustServerCertificate: true },
     };
-
     const connection = await sql.connect(sqlConfig);
 
     const { recordset } = await connection.query`SELECT 1;`;
     expect(recordset).toStrictEqual([{ "": 1 }]);
 
     await connection.close();
+    // }
   });
-  // }
 
-  // uriConnect {
   it("should connect and return a query result with database URI", async () => {
+    // mssqlUriConnect {
     await using container = await new MSSQLServerContainer(IMAGE).acceptLicense().start();
 
     const connectionString = container.getConnectionUri();
@@ -45,12 +38,13 @@ describe("MSSqlServerContainer", { timeout: 180_000 }, () => {
     expect(recordset).toStrictEqual([{ "": 1 }]);
 
     await connection.close();
+    // }
   });
-  // }
 
-  // validPassword {
   it("should connect and return a query result with valid custom password", async () => {
+    // mssqlValidPassword {
     await using container = await new MSSQLServerContainer(IMAGE).acceptLicense().withPassword("I!@M#$eCur3").start();
+    // }
 
     const connectionString = container.getConnectionUri();
     const connection = await sql.connect(connectionString);
@@ -60,24 +54,22 @@ describe("MSSqlServerContainer", { timeout: 180_000 }, () => {
 
     await connection.close();
   });
-  // }
 
-  // invalidPassword {
   it("should throw error with invalid password", async () => {
     const container = new MSSQLServerContainer(IMAGE).acceptLicense().withPassword("password");
     await expect(container.start()).rejects.toThrow(
       Error('Log stream ended and message "/.*Recovery is complete.*/" was not received')
     );
   });
-  // }
 
-  // expressEdition {
   it("should start db with express edition", async () => {
+    // mssqlExpressEdition {
     await using container = await new MSSQLServerContainer(IMAGE)
-      .withWaitForMessage(/.*Attribute synchronization manager initialized*/)
       .acceptLicense()
       .withEnvironment({ MSSQL_PID: "Express" })
+      .withWaitForMessage(/.*Attribute synchronization manager initialized*/)
       .start();
+    // }
 
     const { output, exitCode } = await container.exec([
       "/opt/mssql-tools/bin/sqlcmd",
@@ -94,5 +86,4 @@ describe("MSSqlServerContainer", { timeout: 180_000 }, () => {
     expect(exitCode).toBe(0);
     expect(output).toContain("Express Edition");
   });
-  // }
 });
