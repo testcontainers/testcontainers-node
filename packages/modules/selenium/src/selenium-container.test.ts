081d9c8a3c8e5f55edb98de59a41293e3e291e25
Cristian Greco <cristianrgreco@gmail.com>
2025-07-31 14:36:44 +0100

    Selenium

--- packages/modules/selenium/src/selenium-container.test.ts
+++ packages/modules/selenium/src/selenium-container.test.ts
@@ -2,42 +2,47 @@ import path from "path";
 import { Browser, Builder } from "selenium-webdriver";
 import { GenericContainer } from "testcontainers";
 import tmp from "tmp";
+import { getImage } from "../../../testcontainers/src/utils/test-helper";
 import { SELENIUM_VIDEO_IMAGE, SeleniumContainer } from "./selenium-container";
 
-describe("SeleniumContainer", { timeout: 180_000 }, () => {
-  const browsers = [
-    ["CHROME", process.arch === "arm64" ? `seleniarm/standalone-chromium:112.0` : `selenium/standalone-chrome:112.0`],
-    ["FIREFOX", process.arch === "arm64" ? `seleniarm/standalone-firefox:112.0` : `selenium/standalone-firefox:112.0`],
-  ] as const;
+const browsers = [
+  ["CHROME", process.arch === "arm64" ? getImage(__dirname, 1) : getImage(__dirname, 0)],
+  ["FIREFOX", process.arch === "arm64" ? getImage(__dirname, 3) : getImage(__dirname, 2)],
+] as const;
 
-  browsers.forEach(async ([browser, image]) => {
-    it(`should work for ${browser}`, async () => {
-      await using container = await new SeleniumContainer(image).start();
-      const driver = await new Builder().forBrowser(Browser[browser]).usingServer(container.getServerUrl()).build();
+describe.for(browsers)("SeleniumContainer", { timeout: 30_000 }, ([browser, image]) => {
+  it(`should work for ${browser}`, async () => {
+    // seleniumExample {
+    await using container = await new SeleniumContainer(image).start();
 
-      await driver.get("https://testcontainers.com");
-      expect(await driver.getTitle()).toEqual("Testcontainers");
+    const driver = await new Builder().forBrowser(Browser[browser]).usingServer(container.getServerUrl()).build();
+    await driver.get("https://testcontainers.com");
+    expect(await driver.getTitle()).toEqual("Testcontainers");
 
-      await driver.quit();
-    });
+    await driver.quit();
+    // }
+  });
 
-    it(`should record video and save to disk for ${browser}`, async () => {
-      const container = await new SeleniumContainer(image).withRecording().start();
-      const driver = await new Builder().forBrowser(Browser[browser]).usingServer(container.getServerUrl()).build();
-      await driver.get("https://testcontainers.com");
-      await driver.quit();
-      const stoppedContainer = await container.stop();
+  it(`should record video and save to disk for ${browser}`, async () => {
+    // seleniumVideoExample {
+    const container = await new SeleniumContainer(image).withRecording().start();
 
-      const videoFilePath = tmp.fileSync({ keep: false, prefix: `video-${browser}`, postfix: ".mp4" }).name;
-      const videoFileName = path.basename(videoFilePath);
-      await stoppedContainer.saveRecording(videoFilePath);
+    const driver = await new Builder().forBrowser(Browser[browser]).usingServer(container.getServerUrl()).build();
+    await driver.get("https://testcontainers.com");
 
-      await using ffmpegContainer = await new GenericContainer(SELENIUM_VIDEO_IMAGE)
-        .withCommand(["sleep", "infinity"])
-        .start();
-      await ffmpegContainer.copyFilesToContainer([{ source: videoFilePath, target: `/tmp/${videoFileName}` }]);
-      const { exitCode } = await ffmpegContainer.exec(["ffprobe", `/tmp/${videoFileName}`]);
-      expect(exitCode).toBe(0);
-    });
+    await driver.quit();
+    const stoppedContainer = await container.stop();
+
+    const videoFilePath = tmp.fileSync({ keep: false, prefix: `video-${browser}`, postfix: ".mp4" }).name;
+    const videoFileName = path.basename(videoFilePath);
+    await stoppedContainer.saveRecording(videoFilePath);
+
+    await using ffmpegContainer = await new GenericContainer(SELENIUM_VIDEO_IMAGE)
+      .withCommand(["sleep", "infinity"])
+      .start();
+    await ffmpegContainer.copyFilesToContainer([{ source: videoFilePath, target: `/tmp/${videoFileName}` }]);
+    const { exitCode } = await ffmpegContainer.exec(["ffprobe", `/tmp/${videoFileName}`]);
+    expect(exitCode).toBe(0);
+    // }
   });
 });
